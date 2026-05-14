38810be02638b18ebabf9de8dcc0c1e868b375b6
SirW1zz <justinjovenm@gmail.com>
2026-05-14 22:06:19 +0530

    Completed core functionalities!

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from dotenv import load_dotenv
 load_dotenv()
 access_token=os.environ.get("tok")
 repo_name=os.environ.get("repo_name")
+commit_mssg=os.environ.get("commit_message")
+content=os.environ.get("content")
+
 
 
 auth = Auth.Token(access_token)
@@ -23,6 +26,13 @@ try:
 
     if flag==0:
         print(f"Repo '{repo_name}' not found... crosscheck the name (case sensitive)")
+    
+    repo = g.get_user().get_repo(repo_name)
+    contents = repo.get_contents("README.md")
+    repo.update_file(contents.path, commit_mssg, content, contents.sha)
+
+    print("Completed Successfully..")
+
 except Exception as e:
     print("ERROR!!")
     print("Check your token.. is it in quotes? all perms enabled?")
