2a02314f8d8d6f9aed4b3b4cc92762df6f0fd8ad
SirW1zz <justinjovenm@gmail.com>
2026-05-14 19:00:01 +0530

    Work started on authentication and user verification

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 #Creation of auto commit bot 
+from github import Auth
+from github import Github
+from github import GithubIntegration
+import os
+from dotenv import load_dotenv
+
+auth = Auth.Login("user_login", "password")
+g = Github(auth=auth)
+g.get_user().login
+
+load_dotenv()
+
+user=os.environ.get("user")
+pw=os.environ.get("pass")
+
+print(user,pw)
