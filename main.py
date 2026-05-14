c8b5ac1b207b9bea8ffd57b580c4592515636392
SirW1zz <justinjovenm@gmail.com>
2026-05-14 19:19:52 +0530

    Changed auth method

--- main.py
+++ main.py
@@ -5,13 +5,11 @@ from github import GithubIntegration
 import os
 from dotenv import load_dotenv
 
-auth = Auth.Login("user_login", "password")
+load_dotenv()
+token=os.environ.get("tok")
+
+auth = Auth.Token("access_token")
 g = Github(auth=auth)
 g.get_user().login
 
-load_dotenv()
-
-user=os.environ.get("user")
-pw=os.environ.get("pass")
-
-print(user,pw)
+print(g)
